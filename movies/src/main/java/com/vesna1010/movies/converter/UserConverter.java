f196a76914501430923307e60acf35ca595553ec
Vesna <vesnasdoboj@gmail.com>
2019-08-28 17:59:03 +0200

    Update UserConverter.java

--- movies/src/main/java/com/vesna1010/movies/converter/UserConverter.java
+++ movies/src/main/java/com/vesna1010/movies/converter/UserConverter.java
@@ -2,11 +2,9 @@ package com.vesna1010.movies.converter;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.convert.converter.Converter;
-import org.springframework.stereotype.Component;
 import com.vesna1010.movies.model.User;
 import com.vesna1010.movies.service.UserService;
 
-@Component("userConverter")
 public class UserConverter implements Converter<String, User> {
 
 	@Autowired
