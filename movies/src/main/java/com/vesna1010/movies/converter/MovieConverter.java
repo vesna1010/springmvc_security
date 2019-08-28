d75cd2767ca6e0ff9f3e341bf9a182708ad82fcc
Vesna <vesnasdoboj@gmail.com>
2019-08-28 17:58:39 +0200

    Update MovieConverter.java

--- movies/src/main/java/com/vesna1010/movies/converter/MovieConverter.java
+++ movies/src/main/java/com/vesna1010/movies/converter/MovieConverter.java
@@ -2,11 +2,9 @@ package com.vesna1010.movies.converter;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.convert.converter.Converter;
-import org.springframework.stereotype.Component;
 import com.vesna1010.movies.model.Movie;
 import com.vesna1010.movies.service.MovieService;
 
-@Component("movieConverter")
 public class MovieConverter implements Converter<String, Movie> {
 
 	@Autowired
