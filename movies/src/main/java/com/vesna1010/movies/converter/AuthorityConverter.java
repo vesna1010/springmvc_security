267b411bd57b54c5df6c674ce1c0d8a32eba2e36
Vesna <vesnasdoboj@gmail.com>
2019-08-28 17:58:10 +0200

    Update AuthorityConverter.java

--- movies/src/main/java/com/vesna1010/movies/converter/AuthorityConverter.java
+++ movies/src/main/java/com/vesna1010/movies/converter/AuthorityConverter.java
@@ -2,11 +2,9 @@ package com.vesna1010.movies.converter;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.convert.converter.Converter;
-import org.springframework.stereotype.Component;
 import com.vesna1010.movies.model.Authority;
 import com.vesna1010.movies.service.AuthorityService;
 
-@Component("authorityConverter")
 public class AuthorityConverter implements Converter<String, Authority> {
 
 	@Autowired
