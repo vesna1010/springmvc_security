b924a805c7eb2ce69c001bec1d6c7d0783d90bf5
Vesna S <vesnasdoboj@gmail.com>
2022-06-25 22:23:29 +0200

    Update Genre.java

--- movies/src/main/java/com/vesna1010/movies/enums/Genre.java
+++ movies/src/main/java/com/vesna1010/movies/enums/Genre.java
@@ -1,5 +1,16 @@
 package com.vesna1010.movies.enums;
 
 public enum Genre {
-	ACTION, ADVENTURE, COMEDY, CRIME, DRAMA, FANTASY, HORROR, ROMANCE, THRILLER, WESTERN;
+	
+	ACTION, 
+	ADVENTURE, 
+	COMEDY, 
+	CRIME, 
+	DRAMA, 
+	FANTASY, 
+	HORROR, 
+	ROMANCE, 
+	THRILLER, 
+	WESTERN;
+	
 }
